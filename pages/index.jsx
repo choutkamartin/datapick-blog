1e223b0e575c48071d9a6d39d6cb67dab6efc229
Martin Choutka <martafiixek@gmail.com>
2022-01-22 00:06:50 +0100

    Update the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,12 +6,16 @@ import { sortByDate } from "utils";
 
 export default function Index({ posts }) {
   return (
-    <div>
-      <div className="text-2xl font-bold text-gray-600 mb-3">Blog</div>
-      <div className="grid lg:grid-cols-3 gap-20">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+    <div className="flex flex-col items-center">
+      <div className="text-2xl font-bold text-gray-600 mb-3">Welcome</div>
+      <p>Latest news from Datapick.</p>
+      <div>
+        <h2 className="text-xl font-bold text-gray-600 mb-3">Posts</h2>
+        <div className="grid lg:grid-cols-3 gap-20">
+          {posts.map((post, index) => (
+            <Post key={index} post={post} />
+          ))}
+        </div>
       </div>
     </div>
   );
