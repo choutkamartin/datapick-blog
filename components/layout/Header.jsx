0eafd29ced30364ad5f7f825ece4a37d1baa86b2
Martin Choutka <martafiixek@gmail.com>
2022-01-22 00:06:37 +0100

    Change padding for the header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -14,12 +14,12 @@ export default function Header() {
         />
       </div>
       <div className="flex items-center border">
-        <div className="absolute z-20 left-2/4 -translate-x-2/4 bg-white rounded-full w-24 h-24 lg:w-36 lg:h-36 p-6 lg:p-8 border">
+        <div className="absolute z-20 left-2/4 -translate-x-2/4 bg-white rounded-full w-24 h-24 lg:w-32 lg:h-32 p-4 lg:p-8 border">
           <div className="relative w-full h-full">
             <Image src="/logo.svg" layout="fill" alt="Datapick logo" />
           </div>
         </div>
-        <div className="flex items-center gap-x-8 px-4 py-6 lg:py-8 md:px-8 lg:px-20 xl:px-40 2xl:px-80 ">
+        <div className="flex items-center gap-x-8 px-4 py-4 lg:py-6 md:px-8 lg:px-20 xl:px-40 2xl:px-80 ">
           <nav className="flex justify-between lg:justify-start gap-x-16 items-center w-full">
             <Link href="/">
               <a className="font-medium text-lg text-gray-600 hover:text-black">
